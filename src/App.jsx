9d8551ec75f22eaa80cd34fdc92fc9165f0917a3
keerthikumar <keerthikumarreddyc@gmail.com>
2026-02-22 16:01:24 +0530

    Added first React components,image,and styling

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Img from "./components/Img";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Img />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Img.jsx
@@ -0,0 +1,10 @@
+import chijg from "../assets/chijg.jpg";
+function Img() {
+  return (
+    <div>
+      <img className="img" src={chijg} />
+      <p>chinnu</p>
+    </div>
+  );
+}
+export default Img;
