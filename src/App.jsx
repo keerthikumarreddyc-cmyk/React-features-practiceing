1e049d2b34714f38620c91d6e244908e6b204310
keerthikumar <keerthikumarreddyc@gmail.com>
2026-02-24 16:53:36 +0530

    day:3 Reusable profile component using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Img from "./components/Img";
 import User from "./components/user";
 import "./App.css";
+import Profile from "./components/card";
 
 function App() {
   return (
     <div>
       <Img />
-      <User name="chinnu" role="frontend" location="chicago" />
+      <User gname="chinnu" role="frontend" location="chicago" />
       <User name="pinnu" role="backend" location="newyork" />
       <User name="deepak" role="ui" location="france" />
       <User name="rakesh" role="fullstack" location="london" />
+
+      <Profile name="chinnu" role="frontend" location="chicago" />
+      <Profile name="pinnu" role="backend" location="newyork" />
+      <Profile name="deepak" role="ui" location="france" />
     </div>
   );
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,9 @@
+function Profile({ name, role, location }) {
+  return (
+    <p>
+      {name}, {role}, {location}
+    </p>
+  );
+}
+
+export default Profile;
