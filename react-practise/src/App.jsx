e1acc6799febe10fe94d5a805e6aec0575ae3193
keerthikumar <keerthikumarreddyc@gmail.com>
2026-03-01 13:49:59 +0530

    done project on learned topics

--- react-practise/src/App.jsx
+++ react-practise/src/App.jsx
@@ -2,6 +2,7 @@ import User from "./components/user";
 import user1 from "./assets/user1.png";
 import user2 from "./assets/user2.avif";
 import user3 from "./assets/user3.jpg";
+import user4 from "./assets/user4.jpg";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <User img={user1} name="chinnu" role="devloper" status="active" />
           <User img={user2} name="pinnu" role="automation" status="active" />
           <User img={user3} name="deepu" role="analayst" status="inactive" />
+          <User img={user4} name="anthnoy" role="analayst" status="inactive" />
         </div>
       </div>
     </header>
